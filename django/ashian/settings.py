876118af81c9493aa927c6bd7e956868b6324760
Hossein Radmard <iranradmard1@gmail.com>
2025-01-09 01:29:05 +0330

    migrate to postgres database instead of sqlite3

--- django/ashian/settings.py
+++ django/ashian/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = 'ashian.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ashian",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
